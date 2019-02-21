fc2ba70dfd09535079bd5021dd2813e994cce11b
J A <jarata@gmail.com>
2019-02-21 10:14:20 -0800

    moved and replaced GET / endpoint

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ require('dotenv').config(); // add this line as the first thing to run1
 
 const server = require('./api/server.js');
 
-server.get('/', async (req, res) => {
-  try {
-    const shoutouts = await db('shoutouts');
-    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
-    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  }
-});
-
 // we'll read the port from the server environment if it is there
 // heroku will have the PORT environment variable set
 const port = process.env.PORT || 5000;
